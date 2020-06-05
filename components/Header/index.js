3c0e4ba30e32ea606b7129e3c0da6456a5a98c31
Jessica <duellje@oregonstate.edu>
2020-06-05 11:40:33 -0400

    created header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header(headerObj) {
+    let {date, temp} = headerObj;
+
+    //create each element needed
+    let headerDiv = document.createElement('div');
+    let dateSpan = document.createElement('span');
+    let title = document.createElement('h1');
+    let tempSpan = document.createElement('span');
+
+    //add classes to each element 
+    headerDiv.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    //add content to each element
+    dateSpan.textContent = date;
+    title.textContent = 'Lambda Times';
+    tempSpan.textContent = temp;
+
+    //appends each element onto each other appropiately
+    let elements = [dateSpan, title, tempSpan];
+
+    elements.forEach(item => headerDiv.appendChild(item));
+
+    return headerDiv;
+}
+
+//gets the header-contianer
+let headerContainer = document.querySelector('.header-container');
+let newHeader = Header({date: 'June 5, 2020', temp: '85*'});
+headerContainer.appendChild(newHeader);
